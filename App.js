1cce85f95afc50c534a4009cbdf44be2e9d3819c
Stela Palii <stelasimonapalii@gmail.com>
2024-01-07 11:46:01 -0800

    Add game reset button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useReducer } from "react";
 
 import Board from "./Board";
@@ -9,7 +9,7 @@ import make2dArray from "./make2dArray";
 function makeInitialBoardState() {
   return {
     board: make2dArray(3, 3, null),
-    playerToMove: "X",
+    moveCount: 0,
   };
 }
 
@@ -17,13 +17,17 @@ function reducer(state, action) {
   switch (action.type) {
     case "move": {
       const newBoard = copy2dArray(state.board);
-      newBoard[action.rowIndex][action.columnIndex] = state.playerToMove;
+      newBoard[action.rowIndex][action.columnIndex] =
+        state.moveCount % 2 === 0 ? "X" : "O";
 
       return {
         ...state,
         board: newBoard,
-        playerToMove: state.playerToMove === "X" ? "O" : "X",
+        moveCount: state.moveCount + 1,
       };
+    }
+    case "reset": {
+      return makeInitialBoardState();
     }
   }
 }
@@ -39,6 +43,14 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.heading}>Tic-Tac-Toe</Text>
       <Board board={state.board} dispatch={dispatch} />
+      {state.moveCount === 9 ? (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => dispatch({ type: "reset" })}
+        >
+          <Text style={styles.buttonText}>Reset game</Text>
+        </TouchableOpacity>
+      ) : null}
       <StatusBar style="auto" />
     </View>
   );
@@ -56,5 +68,23 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 600,
     margin: 20,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "blue",
+    borderRadius: 5,
+    color: "white",
+    fontSize: 40,
+    fontWeight: 700,
+    height: 50,
+    justifyContent: "center",
+    marginVertical: 20,
+    padding: 10,
+    width: "60%",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
